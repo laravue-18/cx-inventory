d7db048dd3e04a85cc545c63161a161e7a7752c3
laravue-18 <yurijklimovv@gmail.com>
2021-11-01 02:37:44 +0800

    product model missing

---
+++ app/Models/Product.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['user_id', 'location_id', 'make_id', 'product_model_id', 'part_number', 'colour_id', 'storage_id', 'qty', 'price', 'note'];
+
+    public function productModel(){
+        return $this->belongsTo('App\Models\ProductModel');
+    }
+
+    public function make(){
+        return $this->belongsTo('App\Models\Make');
+    }
+
+    public function supplier(){
+        return $this->belongsTo('App\Models\User', 'user_id');
+    }
+
+    public function location(){
+        return $this->belongsTo('App\Models\Location');
+    }
+
+    public function colour(){
+        return $this->belongsTo('App\Models\Colour');
+    }
+
+    public function storage(){
+        return $this->belongsTo('App\Models\ProductStorage', 'storage_id');
+    }
+
+    public function conditions(){
+        return $this->belongsToMany('App\Models\Condition');
+    }
+
+    public function items(){
+        return $this->hasMany('App\Models\Item');
+    }
+}

afe501cdffab815e11aa96d3de50a58695e5b0e9
laravue-18 <yurijklimovv@gmail.com>
2021-11-16 08:56:20 +0800

    commit

---
+++ database/migrations/2021_11_15_190654_create_transactions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->integer('product_id');
+            $table->integer('qty');
+            $table->float('paid');
+            $table->string('note');
+            $table->string('buyer');
+            $table->string('status');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
